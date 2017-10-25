5ed876c225ad39115076d95a3c4df67857a63b5b
petya230 <petya230@gmail.com>
2017-10-25 18:48:08 +0200

    ops

--- cpu.py
+++ cpu.py
@@ -1,9 +1,4 @@
 import os
 temp = os.popen('cat /sys/class/thermal/thermal_zone0/temp').read()
-#fajl megnyitasa
-#f = open (temp, 'r')
-#sensor = f.readlines()
-#f.close()
-#ertek szetbontas
 cel = (int(float(temp)) / 1000.0)
 print "%.1f C*" % cel
