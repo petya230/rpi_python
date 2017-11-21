892b46570acf896763a1036e0cb11b70d741d878
petya230 <petya230@gmail.com>
2017-11-21 14:01:44 +0100

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,4 +1,4 @@
-#its working on pc too! (tested in ubuntu 16.04)
+#its working on pc too! (tested in ubuntu 14.04 @ 16.04)
 import os
 temp = os.popen('cat /sys/class/thermal/thermal_zone0/temp').read()
 cel = (int(float(temp)) / 1000.0)
