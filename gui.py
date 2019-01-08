03dfd8f6b000539aa1926455be8aa9bf94a90b8d
petya230 <petya230@gmail.com>
2019-01-08 21:36:48 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,31 +1,60 @@
-#coding: utf-8
-
-import os
-temp = os.popen('cat /sys/class/thermal/thermal_zone0/temp').read()
-cel = (int(float(temp)) / 1000.0)
-#print "%.1f C*" % cel
-
+import sys
+import time
 import datetime
+import tkinter
+from tkinter import messagebox
+from tkinter import *
+from random import randint
 
+x = "500"
+y = "100"
+meret = x + "x" + y
 now = datetime.datetime.now()
-asd = str(now.hour) + " : " + str(now.minute)
-import Tkinter
-import tkMessageBox
+ido = str(now.hour) + " : " + str(now.minute)
+top = tkinter.Tk()
+top.geometry(meret)
+top.resizable(0, 0)
 
-top = Tkinter.Tk()
-def cpu():
-   tkMessageBox.showinfo("Fok", cel )
 
-def ido():
-   tkMessageBox.showinfo("Idő", asd)
+def idoButton():
+    messagebox.showinfo("Jelenlegi idő:", ido)
 
-gomb1 = Tkinter.Button(top, text = "CPU hőfok", command = cpu)
-gomb1.pack()
 
-gomb2 = Tkinter.Button(top, text = "Jelenlegi idő", command = ido)
-gomb2.pack()
+def LeszOKJ():
+    leszE = messagebox.askyesno("O KÁ JÉ", "Lesz OKJ-m???")
+    if leszE == True:
+        messagebox.showinfo("yey", "Köszönööööm")
+    if leszE == False:
+        messagebox.showinfo("meh", "KAMU!")
 
-kilep = Tkinter.Button(top, text="Kilép", command=top.quit)
-kilep.pack()
+
+def ablaknyitas():
+    global meret
+    jatek = tkinter.Tk()
+    jatek.geometry(meret)
+    game()
+
+
+def game():
+    gombJ = tkinter.Button(jatek, text="Kapj el", command=jatek.quit())
+    i = 0
+    while i < 10:
+        gombJ.place(x=str(randint(0, int(x))), y=str(randint(0, int(y))))
+        time.sleep(1)
+        i = i+1
+    jatek.mainloop()
+
+
+gomb1 = tkinter.Button(top, text="OKJ", command=LeszOKJ)
+gomb1.place(relx=0.0, rely=0.0, anchor="nw", bordermode="outside")
+
+gomb2 = tkinter.Button(top, text="Jelenlegi idő", command=idoButton)
+gomb2.place(relx=1, rely=0.0, anchor="ne", bordermode="outside")
+
+jatek = tkinter.Button(top, text="Játék", command=ablaknyitas)
+jatek.place(relx=0.5, rely=0.0, anchor="n", bordermode="outside")
+
+kilep = tkinter.Button(top, text="Kilép", command=top.quit)
+kilep.place(relx=0.5, rely=1, bordermode="outside", anchor="s")
 
 top.mainloop()
